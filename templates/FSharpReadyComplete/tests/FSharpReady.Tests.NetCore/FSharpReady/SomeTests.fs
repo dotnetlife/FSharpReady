dcaf5d33bb916939c9ff8501c381455a92eb5068
Heinrich Weil <hl.weil@t-online.de>
2020-09-17 16:04:22 +0200

    add testing to FSharpReadyComplete

---
+++ templates/FSharpReadyComplete/tests/FSharpReady.Tests.NetCore/FSharpReady/SomeTests.fs
@@ -0,0 +1,17 @@
+﻿module SomeTests
+
+open FSharpReady
+open Expecto
+
+
+[<Tests>]
+let testStuff = 
+    testList "AminoAcids" [
+        testCase "example" (fun () -> 
+            let testSymbols = ['a','b','c']
+            Expect.sequenceEqual
+                testSymbols
+                ['a','b','c']
+                "wrooong"
+        )
+    ]
---
+++ templates/FSharpReadyComplete/tests/FSharpReady.Tests.NetCore/Main.fs
@@ -0,0 +1,10 @@
+﻿module FSharpReady.Tests.NetCore
+
+open Expecto
+
+[<EntryPoint>]
+let main argv =
+
+    //FSharpReady core tests
+    Tests.runTestsWithCLIArgs [] argv SomeTests.testStuff         |> ignore
+    0
